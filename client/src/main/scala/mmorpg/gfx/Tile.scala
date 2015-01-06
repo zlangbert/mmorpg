f7bd26755b1ec21271644eb70a52ecff24d09d89
Zach Langbert <zachl44@gmail.com>
2015-01-05 23:52:15 -0600

    Don't draw tiles off screen

--- client/src/main/scala/mmorpg/gfx/Tile.scala
+++ client/src/main/scala/mmorpg/gfx/Tile.scala
@@ -5,6 +5,7 @@ import org.scalajs.dom.HTMLImageElement
 
 class Tile(id: Int, img: HTMLImageElement, tileset: Tileset, offset: Vec) extends Renderable {
   override def renderAt(x: Int, y: Int)(implicit ctx: RenderingContext): Unit = {
+    if (x >= ctx.canvas.width || y >= ctx.canvas.height) return
     ctx.drawImage(img, offset.x, offset.y, tileset.tileSize, tileset.tileSize,
       x, y, tileset.tileSize, tileset.tileSize)
   }
