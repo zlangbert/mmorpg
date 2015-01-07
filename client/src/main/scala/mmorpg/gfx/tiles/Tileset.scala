c9693609d3b7e6c7529f631f0d9cb4186c6f87e5
Zach Langbert <zachl44@gmail.com>
2015-01-07 16:09:16 -0600

    Protect Tileset from being instantiated directly
    You probably wouldn't want a Tileset unless its image was loaded and this guarantees that won't happen

--- client/src/main/scala/mmorpg/gfx/tiles/Tileset.scala
+++ client/src/main/scala/mmorpg/gfx/tiles/Tileset.scala
@@ -6,9 +6,38 @@ import org.scalajs.dom.HTMLImageElement
 
 import scala.async.Async._
 import scala.collection.mutable
+import scala.concurrent.Future
 import scala.scalajs.concurrent.JSExecutionContext.Implicits.queue
 
-class Tileset(underlying: Tmx.Tileset, img: HTMLImageElement) {
+trait Tileset {
+
+  /**
+   * Gets a Tile from this tileset by its global id
+   * @param globalId The global id of the tile
+   * @return The tile
+   */
+  def apply(globalId: Int): Tile
+
+  /**
+   * The first global id for this tileset
+   * @return
+   */
+  def firstGid: Int
+
+  /**
+   * The size of a tile in pixels
+   * @return Tile size in pixels
+   */
+  def tileSize: Int
+
+  /**
+   * The width of the tileset image in pixels
+   * @return Image width in pixels
+   */
+  def imageWidth: Int
+}
+
+private class TilesetImpl(underlying: Tmx.Tileset, img: HTMLImageElement) extends Tileset {
 
   /**
    * Cache for created tiles
@@ -18,11 +47,6 @@ class Tileset(underlying: Tmx.Tileset, img: HTMLImageElement) {
   //load animated tiles into the cache
   loadAnimatedTiles()
 
-  /**
-   * Gets a Tile from this tileset by its global id
-   * @param globalId The global id of the tile
-   * @return The tile
-   */
   def apply(globalId: Int): Tile = {
     val localId = globalId - firstGid
     tileCache.getOrElseUpdate(localId, {
@@ -30,22 +54,10 @@ class Tileset(underlying: Tmx.Tileset, img: HTMLImageElement) {
     })
   }
 
-  /**
-   * The first global id for this tileset
-   * @return
-   */
   def firstGid: Int = underlying.firstGid
 
-  /**
-   * The size of a tile in pixels
-   * @return Tile size in pixels
-   */
   def tileSize: Int = underlying.tileWidth
 
-  /**
-   * The width of the tileset image in pixels
-   * @return Image width in pixels
-   */
   def imageWidth: Int = underlying.imageWidth
 
   private def loadAnimatedTiles(): Unit = {
@@ -57,10 +69,10 @@ class Tileset(underlying: Tmx.Tileset, img: HTMLImageElement) {
 }
 
 object Tileset {
-  def apply(mapKey: String)(underlying: Tmx.Tileset) = {
+  def apply(mapKey: String)(underlying: Tmx.Tileset): Future[Tileset] = {
     async {
       val img = await(ImageLoader(s"/maps/$mapKey/${underlying.imagePath}"))
-      new Tileset(underlying, img)
+      new TilesetImpl(underlying, img)
     }
   }
 }
