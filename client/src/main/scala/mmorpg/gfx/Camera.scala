e4a7820618bf593c9dc92d9bbd8817c05666a44b
Zach Langbert <zachl44@gmail.com>
2015-01-11 16:05:19 -0600

    Small camera cleanup

--- client/src/main/scala/mmorpg/gfx/Camera.scala
+++ client/src/main/scala/mmorpg/gfx/Camera.scala
@@ -12,14 +12,28 @@ class Camera(val size: Vec) {
   def position = _position
   def position_=(p: Position) = _position = p
 
-  def worldToScreen(x: Int, y: Int): (Int, Int) = {
+  /**
+   * Converts world coordinates to screen coordinates based on
+   * the position of this camera
+   * @param x
+   * @param y
+   * @return The screen coordinates
+   */
+  def worldToScreen(x: Int, y: Int): Position = {
     val (px, py) = position
     val screenX = x - (px - Client.canvas.width / 2)
     val screenY = y - (py - Client.canvas.height / 2)
     screenX -> screenY
   }
 
-  def screenToWorld(x: Int, y: Int): (Int, Int) = {
+  /**
+   * Converts screen coordinates to world coordinates based on
+   * the position of this camera
+   * @param x
+   * @param y
+   * @return The world coordinates
+   */
+  def screenToWorld(x: Int, y: Int): Position = {
     val (px, py) = position
     val worldX = x + (px - Client.canvas.width / 2)
     val worldY = y + (py - Client.canvas.height / 2)
@@ -27,12 +41,12 @@ class Camera(val size: Vec) {
   }
 
   /**
-   * Clamp coordinates to the top left of the tile
+   * Clamps coordinates to the top left of the tile
    * @param x
    * @param y
    * @return
    */
-  def clampToGrid(x: Int, y: Int): (Int, Int) = {
+  def clampToGrid(x: Int, y: Int): Position = {
     val clampedX = math.floor(x / 48).toInt * 48
     val clampedY = math.floor(y / 48).toInt * 48
     clampedX -> clampedY
